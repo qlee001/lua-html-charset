92f39355aa6d3e564113f7db11b859f2f1539168
qleex001 <qleex001@gmail.com>
2016-03-22 21:55:30 +0800

    add file

---
+++ charset.lua
@@ -0,0 +1,50 @@
+local str_lower = string.lower
+local str_match = string.match
+local resp_headers = ngx.resp.get_headers
+local re_match = ngx.re.match
+
+
+local function get_charset_from_header()
+    local header = resp_headers()
+    local content_type = header["Content-Type"]
+    if not content_type then
+        return nil
+    end
+
+    local res, err = re_match(content_type, '[\\w/]+;\\s*charset=(\\S+)')
+    if res then
+        local charset = str_lower(res[1])
+        return charset
+    end
+
+    return nil
+end
+
+local function get_charset_from_html(html)
+    local res, err = re_match(html, '<meta\\s+http-equiv="Content-Type"\\s+content="[\\w/]+;\\s*charset=[\\s]*([^\\s"]+)"', 'si')
+    if res then
+        local charset = str_lower(res[1])
+        return charset
+    end
+
+    return nil
+end
+
+
+--Without Nginx_lua api
+local function get_charset_from_html2(html)
+    --local res, err = str_match(html, '<meta%s+http-equiv="Content-Type"%s+content="[%w/]+;%s*charset=[%s]*([^%s"]+)"')
+    local res, err = str_match(html, '<meta%s+http%-equiv="Content%-Type"%s+content="[%w/]+;%s*charset=[%s]*([^%s"]+)"')
+    if res then
+        local charset = str_lower(res)
+        return charset
+    end
+
+    return nil
+end
+
+local _M = { get_charset_from_header = get_charset_from_header,
+             get_charset_from_html = get_charset_from_html,
+             get_charset_from_html2 = get_charset_from_html2,
+           }
+return _M
